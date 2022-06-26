2c94bf5e042e8a8681f5a8c3e0e4edb61bcc708d
hktrollgames <hktrollgames@gmail.com>
2022-06-26 13:49:24 +0900

    점프

---
+++ Assets/Player.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Vector2 jumpForce = new Vector2(0, 7);
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+    void Update()
+    {
+        if (Input.anyKeyDown)
+            rb.AddForce(jumpForce, ForceMode2D.Impulse);
+    }
+}
